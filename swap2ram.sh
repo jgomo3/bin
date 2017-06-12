0f7ca24375395dcafc5e705222eec6b66588b977
Jesús Gómez <jgomo3@gmail.com>
2017-06-12 13:17:28 -0230

    Create swap2ram.sh to clean the swap if able
    From the Ubuntu wiki Swap FAQ
    https://help.ubuntu.com/community/SwapFaq?action=recall&rev=100
    
    It checks first if there is enough memory to get swap content there.

---
+++ swap2ram.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# From the Ubuntu wiki Swap FAQ
+# https://help.ubuntu.com/community/SwapFaq?action=recall&rev=100
+
+mem=$(LC_ALL=C free  | awk '/Mem:/ {print $4}')
+swap=$(LC_ALL=C free | awk '/Swap:/ {print $3}')
+
+if [ $mem -lt $swap ]; then
+    echo "ERROR: not enough RAM to write swap back, nothing done" >&2
+    exit 1
+fi
+
+swapoff -a &&
+swapon -a
